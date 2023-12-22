3b2f69598aa012407142227b8490bdc4d3a193da
Thang Pham <thang10262001@gmail.com>
2023-12-22 08:38:37 -0500

    Added function to write data to the USART2_DR register to be sent out.

---
+++ Src/uart_engine.c
@@ -0,0 +1,24 @@
+/*
+ * UART_Engine.c
+ *
+ *  Created on: Dec 21, 2023
+ *      Author: Thang Pham
+ * 
+ *  File that contains all of the UART associated functions and operations.
+ * 
+ */
+
+#include "common.h"
+#include "uart_init.h"
+
+/*
+ * Function to write data onto the UART data register to be sent out.
+ */
+
+void USART2_Write(int data){
+
+    /* Wait until the TX Buffer is empty*/
+    while (!(USART2_TX_BUFF_READY)) {}
+    USART2->DR = (data & 0xFF);
+
+}
