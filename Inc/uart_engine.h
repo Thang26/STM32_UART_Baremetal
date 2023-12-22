4c6bc5448608c0c7f55dc8f9e54b3b820c0aae00
Thang Pham <thang10262001@gmail.com>
2023-12-22 08:37:38 -0500

    Added UART_Engine.h file.

---
+++ Inc/uart_engine.h
@@ -0,0 +1,16 @@
+/*
+ * UART_Engine.h
+ *
+ *  Created on: Dec 22, 2023
+ *      Author: Thang Pham
+ * 
+ *  Header file associated with the UART_Engine.c file.
+ * 
+ */
+
+/*
+ *  Function Prototypes
+ */
+
+void USART2_Init(void);
+void USART2_Write(int data);
