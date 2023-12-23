e42785a003de84689370349087e05bfc47b8f1c2
Thang Pham <thang10262001@gmail.com>
2023-12-23 13:38:40 -0500

    Added UART write function for USART1 and USART3.

--- Src/uart_engine.c
+++ Src/uart_engine.c
@@ -12,7 +12,33 @@
 #include "uart_init.h"
 
 /*
- * Function to write data onto the UART data register to be sent out.
+ * Function to write data onto the UART1 data register to be sent out.
+ */
+
+void USART1_Write(int data){
+
+    /* Wait until the TX Buffer is empty*/
+    while (!(USART1_TX_BUFF_READY)) {}
+    USART1->DR = (data & 0xFF);
+}
+
+/*
+ * Function to write a string of data onto the UART2 data register to be sent out.
+ */
+
+void USART1_WriteString(char *data){
+
+    while(*data != '\0'){
+        
+        /* Wait until the TX Buffer is empty*/
+        while (!(USART1_TX_BUFF_READY)) {}
+        USART1_Write((int)(*data));
+        data++;
+    }
+}
+
+/*
+ * Function to write data onto the UART2 data register to be sent out.
  */
 
 void USART2_Write(int data){
@@ -23,7 +49,7 @@ void USART2_Write(int data){
 }
 
 /*
- * Function to write a string of data onto the UART data register to be sent out.
+ * Function to write a string of data onto the UART2 data register to be sent out.
  */
 
 void USART2_WriteString(char *data){
@@ -33,6 +59,32 @@ void USART2_WriteString(char *data){
         /* Wait until the TX Buffer is empty*/
         while (!(USART2_TX_BUFF_READY)) {}
         USART2_Write((int)(*data));
+        data++;
+    }
+}
+
+/*
+ * Function to write data onto the UART3 data register to be sent out.
+ */
+
+void USART3_Write(int data){
+
+    /* Wait until the TX Buffer is empty*/
+    while (!(USART3_TX_BUFF_READY)) {}
+    USART3->DR = (data & 0xFF);
+}
+
+/*
+ * Function to write a string of data onto the UART3 data register to be sent out.
+ */
+
+void USART3_WriteString(char *data){
+
+    while(*data != '\0'){
+        
+        /* Wait until the TX Buffer is empty*/
+        while (!(USART3_TX_BUFF_READY)) {}
+        USART3_Write((int)(*data));
         data++;
     }
 }
