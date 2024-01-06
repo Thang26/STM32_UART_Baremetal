ba66f6c0e19164e8708e0a7f84ba4349fdd0feb0
Thang Pham <thang10262001@gmail.com>
2024-01-06 01:12:47 -0500

    Fixed an error in arranging the bits within slot 5 - 7 of the first transmission array.

--- Src/uart_engine.c
+++ Src/uart_engine.c
@@ -89,6 +89,7 @@ void USART3_WriteString(char *data){
     }
 }
 
+//TODO: Write a description of what this function does.
 void ENCODE_FUNC(UINT16 dataValue, UINT8 *first_send, UINT8 *second_send, UINT8 *first_transmit, UINT8 *second_transmit){
 
     UINT8 bit_array[10] = {};
@@ -104,12 +105,11 @@ void ENCODE_FUNC(UINT16 dataValue, UINT8 *first_send, UINT8 *second_send, UINT8 
     for(index = 0; index < 10; index++){
 
         /*
-         *  Bit_array is stored as 000 1110101.
-         *  (LSB)0001110101(MSB) where this is reversed of 1010111000.
+         *  This part puts the individual bits of the data we're trying to send into their correct slots of the two transmission arrays.
          *  Looks at the first three entries of bit_array and puts that into the first transmission (slot 5 - 7).
          */
         if(index < FIRST_THREE_DIGITS){
-            first_send[7-index] = bit_array[index];
+            first_send[5+index] = bit_array[index];
         }
 
         /*  Looks at the remaining seven entries of bit_array and puts that into the second transmission (slot 1 - 7). */
