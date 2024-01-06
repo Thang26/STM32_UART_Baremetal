a6bdf5267894884c1e43980cd1a294c9feeda7e9
Thang Pham <thang10262001@gmail.com>
2024-01-06 01:13:14 -0500

    Implemented the Python's side of reading in the two transmissions and reconstructing the original packet of 10-bit data.

--- Pyserial_COM.py
+++ Pyserial_COM.py
@@ -17,21 +17,78 @@ serial.STOPBITS_ONE
 
 # Function to read data from COM5 port.
 def getData():
-    data = COMvalue.read()
-    return data
+
+    flag = 0
+    check = 0
+    #   Expand on this later cause I'm tired. But basically, it read the second transmission FIRST,
+    #   then paired it up with the first transmission of the NEXT packet.
+    #   Was very hard to detect because between 690 and 693, the second packet does not differ.
+    binary_list = []
+    first_transmission = []
+    second_transmission = []
+    ADC_value = []
+
+    while flag != 2:
+        data = bin(int((COMvalue.read()).hex(), 16))[2:].zfill(8)
+
+        # print("\n" + "Data is obtained.")
+        # print(data)
+
+        # Splits the binary data into individual bits within a list. This is so each individual bit can be accessed.
+        binary_list = [int(x) for x in str(data)]
+        
+        if (binary_list[0] == 0):
+            first_transmission = binary_list[0:8]
+            # print("First transmission got assigned.")
+            # print(first_transmission)
+            flag += 1
+            check = 1
+        elif(check == 1):
+            second_transmission = binary_list[0:8]
+            # print("Second transmission got assigned.")
+            # print(second_transmission)
+            flag += 1
+            check = 0
+
+    # For syntax of list[-1:-9:-1], it's actually [-9, -8, -7, -6 ... -1] and it's interating thu by doing element -1 + (-1)
+    # print("\n" + "Printing first_transmission[-1:-5:-1]: " + str(first_transmission[-1:-4:-1]))
+    # print("\n" + "Printing second_transmission[-1:-9:-1]: " + str(second_transmission[-1:-8:-1]))
+    
+    ADC_value = convert((second_transmission[-1:-8:-1] + first_transmission[-1:-4:-1]))
+
+    # print("\n" + "Printing ADC value")
+    # print(ADC_value)
+
+    return ADC_value
+
+# Function to join together the list of binary values into a singular binary.
+def convert(list):
+     
+    # Converting integer list to string list
+    s = [str(i) for i in list]
+     
+    # Join list items then convert them into base 10.
+    result = int("".join(s), base=2)
+     
+    return(result)
+
+# def main():
+#     while True:
+#         getData()
+# main()
 
 style.use('fivethirtyeight')
 
 # Counts upward by 1, serves as our x-axis basis
 index = count()
-max_length = 50 # Max length defines the size of the x-axis.
+max_length = 1000 # Max length defines the size of the x-axis.
 xs, ys = [], []
 
 # Creating the intial plot figure.
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 ax.set_xlim([0, max_length])
-ax.set_ylim([0,15])
+ax.set_ylim([0,1024])
 
 image, = ax.plot([],[])
 
@@ -41,7 +98,7 @@ def update(i):
 
     # Gets our next values for the x and y axis.
     xs.append(next(index))
-    ys.append(int(getData().hex(),16))
+    ys.append(getData())
 
     # Set those new values to be plotted.
     image.set_data(xs, ys)
@@ -54,7 +111,7 @@ def update(i):
 
     return image,
 
-ani = FuncAnimation(fig, update, interval = 30, blit=False)
+ani = FuncAnimation(fig, update, interval = 10, blit=False)
 
 plt.tight_layout()
 plt.show()
