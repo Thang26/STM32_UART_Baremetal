434866cbff7d43c892bce22a16304415c0dafd64
Thang Pham <thang10262001@gmail.com>
2024-01-06 01:13:48 -0500

    Implemented functionality to loop through an array of values and sending them out via UART.

--- Src/main.c
+++ Src/main.c
@@ -27,25 +27,43 @@
 
 int main(void)
 {
-  USART3_Init();
-  DELAY_OPM_TIMER_INIT(100);
-  UINT16 sineWave[1] = {696};
+
+  const UINT16 sineWave[100] = {
+    348, 370, 392, 413, 435, 456, 476, 496, 516, 534,
+    553, 570, 586, 602, 616, 630, 642, 653, 663, 672,
+    679, 685, 690, 693, 695, 696, 695, 693, 690, 685,
+    679, 672, 663, 653, 642, 630, 616, 602, 586, 570,
+    553, 534, 516, 496, 476, 456, 435, 413, 392, 370,
+    348, 326, 304, 283, 261, 240, 220, 200, 180, 162,
+    143, 126, 110, 94, 80, 66, 54, 43, 33, 24,
+    17, 11, 6, 3, 1, 0, 1, 3, 6, 11,
+    17, 24, 33, 43, 54, 66, 80, 94, 110, 126,
+    143, 162, 180, 200, 220, 240, 261, 283, 304, 326
+  };
+
   UINT8 first_send[SIZE_OF_BYTE] = {0, 0, 0, 0, 1};
   UINT8 second_send[SIZE_OF_BYTE] = {1, };
   UINT8 first_transmit, second_transmit;
 
+  /*
+   *  Initialization Routine
+   */
+  USART3_Init();
+  DELAY_OPM_TIMER_INIT(100);
+
   while(1){
 
     //Does UART send the MSB or LSB first?
+    for(int i = 0; i < sizeof(sineWave)/sizeof(sineWave[0]); i++){
+      ENCODE_FUNC(sineWave[i], first_send, second_send, &first_transmit, &second_transmit);
 
-    ENCODE_FUNC(sineWave[0], first_send, second_send, &first_transmit, &second_transmit);
+      USART3_Write(first_transmit);
+      DELAY_OPM_TIMER_UPDATE(100);
+      while(!(TIM2_COMPLETE_EVENT)){}
 
-    USART3_Write(first_transmit);
-    DELAY_OPM_TIMER_UPDATE(5000);
-    while(!(TIM2_COMPLETE_EVENT)){}
-
-    USART3_Write(second_transmit);
-    DELAY_OPM_TIMER_UPDATE(5000);
-    while(!(TIM2_COMPLETE_EVENT)){}
+      USART3_Write(second_transmit);
+      DELAY_OPM_TIMER_UPDATE(100);
+      while(!(TIM2_COMPLETE_EVENT)){}
+    }
   }
 }
